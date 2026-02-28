d1e858b1437d3cb683e4f81e666743f3932d55be
Anshumaan Khare <anshumaankhare2403@gmail.com>
2026-02-28 22:12:26 +0530

    dart

--- DART_REVESION/06_Null Safety.dart
+++ DART_REVESION/06_Null Safety.dart
@@ -1,3 +1,30 @@
+// void main(){
+// //   String str = null;
+// //   print(str.length);
+
+// //   List<int> num = null;
+// //   num.add(10);
+// //   print(num);
+
+// //   String? str; 
+// //   print(str!.length); 
+
+
+// //   String ss = null;
+// //   print(ss);
+
+
+// }
+
+
 void main(){
-  
+    int? num;
+    print(num);
+    List<int> arr = [];
+    arr.add(100);
+    print(arr);
+    
+
+
+
 }
---
+++ DART_REVESION/07_collection.dart
@@ -0,0 +1,10 @@
+void main(){
+  List<int> arr = [1,2,3,4];
+  print(arr);
+  var ll = <int>[];
+  ll.add(10);
+  ll.add(10);
+  ll.add(10);
+  print(ll);
+  
+}
